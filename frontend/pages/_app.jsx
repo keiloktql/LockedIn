371ae7a90dc7f5c7a4ef2938ad87e7d64e52188e
keiloktql <tkl48leon@gmail.com>
2025-02-15 22:20:32 +0800

    [feat] add nprogress

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -1,5 +1,16 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import "@/styles/globals.css";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    NProgress.configure({ showSpinner: false });
+    router.events.on("routeChangeStart", () => NProgress.start());
+    router.events.on("routeChangeComplete", () => NProgress.done());
+    router.events.on("routeChangeError", () => NProgress.done());
+  }, []);
   return <Component {...pageProps} />;
 }
