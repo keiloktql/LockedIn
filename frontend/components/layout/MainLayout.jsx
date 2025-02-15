66a0557e12b98b64ccf5d197f8cb7c082ddee089
keiloktql <tkl48leon@gmail.com>
2025-02-15 19:36:49 +0800

    [feat] footer copmonent

--- frontend/components/layout/MainLayout.jsx
+++ frontend/components/layout/MainLayout.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "@/components/layout/Header";
 import SEO from "./SEO";
-// import Footer from "@/components/layout/Footer";
+import Footer from "@/components/layout/Footer";
 
 const MainLayout = ({ children, title, className }) => (
   <>
     <SEO title={title} />
     <Header />
     <main className={`min-h-[calc(100vh-80px)] ${className}`}>{children}</main>
-    {/* <Footer /> */}
+    <Footer />
   </>
 );
 
